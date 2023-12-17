01079ffcf25f319f10fb22b4adc42ebb4b2ee5c4
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2023-12-17 09:14:20 +0530

    Created the basics

---
+++ backend/HandmadeI/src/main/java/com/HandmadeI/HandmadeI/HandmadeIApplication.java
@@ -0,0 +1,13 @@
+package com.HandmadeI.HandmadeI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandmadeIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandmadeIApplication.class, args);
+	}
+
+}
---
+++ backend/HandmadeI/src/test/java/com/HandmadeI/HandmadeI/HandmadeIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HandmadeI.HandmadeI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandmadeIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
