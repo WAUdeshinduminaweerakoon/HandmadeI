0116e0e8c3b31a818b8f6fc133794bc49bc03325
WAUdeshinduminaweerakoon <udeshidumina@gmail.com>
2023-12-23 10:35:44 +0530

    error,Error starting ApplicationContext. To display the condition evaluation report re-run your application with 'debug' enabled.

--- backend/HandmadeI/src/main/java/com/HandmadeI/HandmadeI/Service/UserService.java
+++ backend/HandmadeI/src/main/java/com/HandmadeI/HandmadeI/Service/UserService.java
@@ -1,7 +1,9 @@
 package com.HandmadeI.HandmadeI.Service;
 
 import com.HandmadeI.HandmadeI.Dto.UserDTO;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface UserService {
     String addUser(UserDTO userDTO);
 
